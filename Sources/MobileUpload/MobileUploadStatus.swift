c3521a4852e9ab7f407efe339b0e66cce9ab2be8
Michael Rutherford <doomiigo@gmail.com>
2021-06-20 12:46:56 -0400

    Rewrote the eoTouch sync UI. It's now using async/await and is designed only to handled downloads.
    Uploads will be handled throughout eoTouch (e.g. uploading a pre-sell order when it's done).
    
    Note that the code for the new MobileDownloadWebService is actually living inside SyncHomeView. This is because I can't yet target iOS 15 from a package.

--- Sources/MobileUpload/MobileUploadStatus.swift
+++ Sources/MobileUpload/MobileUploadStatus.swift
@@ -5,10 +5,10 @@
 //  Created by Michael Rutherford on 2/16/21.
 //
 
-import Foundation
-
-enum MobileUploadStatus : String {
-    case Open = "o"
-    case Closed = "c"
-    case Finalized = "f"
-}
+//import Foundation
+//
+//public enum MobileUploadStatus : String {
+//    case Open = "o"
+//    case Closed = "c"
+//    case Finalized = "f"
+//}
